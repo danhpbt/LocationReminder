e9e948b2655455c21f118c249a6d986e32c568f4
Danh Phan <pbtdanh@yahoo.com>
2022-01-21 01:31:27 -0800

    * unit test

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -6,22 +6,39 @@ import com.udacity.project4.locationreminders.data.dto.Result
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private val reminderDTOs = mutableListOf<ReminderDTO>()
+    private var foreceError = false
+
+    fun setForeceError(value: Boolean) {
+        foreceError = value
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (foreceError)
+            return Result.Error("Test Error")
+
+        return Result.Success(reminderDTOs)
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminderDTOs.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (foreceError)
+            return Result.Error("Test Error")
+
+        for (reminder in reminderDTOs) {
+            if (reminder.id == id) {
+                return Result.Success<ReminderDTO>(reminder)
+            }
+        }
+        return Result.Error("Can not find the reminder with id $id.", -1)
+
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminderDTOs.clear()
     }
 
 
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,66 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.app.Application
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.ExpectFailure.assertThat
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.util.MainCoroutineRule
+import com.udacity.project4.locationreminders.util.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: RemindersListViewModel
+    private lateinit var dataSource: FakeDataSource
+
+    @Before
+    fun init() {
+        dataSource = FakeDataSource()
+        viewModel = RemindersListViewModel(Application(),dataSource)
+    }
+
+    @After
+    fun stop() {
+        stopKoin()
+    }
+
+    @Test
+    fun getReminderList_listOneElements() = mainCoroutineRule.runBlockingTest {
+        val reminder = ReminderDTO("Title 1", "Description 1", "Location 1", 0.0, 0.0)
+        dataSource.saveReminder(reminder)
+
+        viewModel.loadReminders()
+
+        val list = viewModel.remindersList.getOrAwaitValue()
+        assertThat(list,(not(nullValue())))
+
+        assertEquals(2,list.size)
+    }
+
+    @Test
+    fun loadReminders_Error() = mainCoroutineRule.runBlockingTest {
+        dataSource.setForeceError(true)
+        viewModel.loadReminders()
+        val snackBarMessage = viewModel.showSnackBar.getOrAwaitValue()
+        assertEquals("Test Error",snackBarMessage)
+    }
 
 }
